0a0cafa3c51d723765b0d8a81092ee4be1e859f1
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-07 18:11:25 -0400

    update to quizzes schema and seed files to add new fields

--- seeds/02_quizzes_seeds.sql
+++ seeds/02_quizzes_seeds.sql
@@ -1,7 +1,7 @@
-INSERT INTO quizzes (id, owner_id, title, create_date, is_private)
+INSERT INTO quizzes (id, owner_id, title, description, category, image_url, create_date, is_private)
 VALUES
-(1, 1, 'Canadian Capital Cities', '2022-02-12', false),
-(2, 4, 'Bakery Trivia', '2022-04-06', false),
-(3, 2, 'Skill Testing Questions', '2021-06-25', false),
-(4, 3, 'Beer Trivia', '2021-10-28', true);
+(1, 1, 'Canadian Capital Cities', 'This quiz will challenge your knowledge of Canadian geography.  Challenge your friends to see if anyone can name all of the provincial/territorial capitals in this great country.', '', '', '2022-02-12', false),
+(2, 4, 'Bakery Trivia', 'If you''re ready to challenge your baking knowledge, this is the quiz for you.  Mainly bread related questions with a few twists.  Enjoy!', '', '', '2022-04-06', false),
+(3, 2, 'Skill Testing Questions', 'Calling all math fans - this is a fun little True/False quiz that will challenge your math skills.  Can you do it without a calculator? I dare you!', '', '', '2021-06-25', false),
+(4, 3, 'Beer Trivia', 'What''s not to love about a quiz about beer?  This is a good mix of history and trivia.  It''s super fun and you may even learn a few things about this wonderful beverage.  Cheers!', '', '', '2021-10-28', true);
 
