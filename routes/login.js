351516e9c64cc46217032c237701a62b20ee7e38
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-08 19:44:21 -0400

    Add login page

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,29 @@ const router  = express.Router();
 module.exports = (db) => {
   router.get("/", (req, res) => {
     res.render("login");
-    })
+  });
+  //handle when a user clicks login in the login form
+  router.post("/", (req, res) => {
+    db.query(`SELECT * from users WHERE email=$1`, [req.body.email])
+      .then(data => {
+        //if the user exists, set a cookie and load the index page
+        if (data.rows.length > 0) {
+          res.cookie("email", data.rows[0].email);
+          res.redirect("index");
+        } else {
+          //if the user doesn't exist, send them to the error page
+          const templateVars = { "errorMessage": "Invalid username" };
+          res.render("error", templateVars);
+        }
+
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+
   return router;
-  };
+};
