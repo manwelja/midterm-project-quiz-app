1a589a64403de30aadf430d093a393c709524c0a
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-08 06:10:00 -0400

    fixed relative path issue

--- db/seeds/00_run_all_seeds.sql
+++ db/seeds/00_run_all_seeds.sql
@@ -1,5 +1,5 @@
-\i seeds/01_users_seeds.sql;
-\i seeds/02_quizzes_seeds.sql;
-\i seeds/03_questions_seeds.sql;
-\i seeds/04_attempts_seeds.sql;
-\i seeds/05_answers_seeds.sql;
+\i db/seeds/01_users_seeds.sql;
+\i db/seeds/02_quizzes_seeds.sql;
+\i db/seeds/03_questions_seeds.sql;
+\i db/seeds/04_attempts_seeds.sql;
+\i db/seeds/05_answers_seeds.sql;
