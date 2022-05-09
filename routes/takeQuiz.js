f91c2a3897c7e1382f47140f3f157fa0916e492e
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-09 18:14:05 -0400

    Connect takeQuiz post to the attempts database

--- routes/takeQuiz.js
+++ routes/takeQuiz.js
@@ -7,7 +7,6 @@ module.exports = (db) => {
     db.query(`SELECT questions.*, quizzes.* FROM questions JOIN quizzes on quizzes.id = quiz_id WHERE quiz_id = $1 ORDER BY question_number`, [req.params.id])
       .then(data => {
         const questions = data.rows;
-        console.log(questions);
         res.render("takeQuiz", {questions: questions}); //res.json({ users });
       })
       .catch(err => {
@@ -15,6 +14,55 @@ module.exports = (db) => {
           .status(500)
           .json({ error: err.message });
       });
-    })
+  });
+
+
+   router.post("/:id", (req, res) => {
+
+      db.query(`SELECT * FROM questions WHERE quiz_id = $1`, [req.params.id])
+        .then(data => {
+        let quizId = req.params.id;
+        let count = 0;
+        let numCorrect = 0;
+        let score = 0;
+        //Put result into the attempt table
+        for (let answer in req.body) {
+          if (data.rows[count].correct_answer === req.body[answer])
+          {
+            numCorrect += 1;
+          }
+          count += 1;
+        }
+        score = Math.ceil((numCorrect/count) * 100);
+
+        const queryString = `
+        SELECT * FROM users WHERE email = $1;
+        `;
+        db.query(queryString, [req.cookies.email])
+          .then((result) => {
+          //If no errors were encountered add the new user to the database
+
+            const queryString = `
+             INSERT INTO attempts (quiz_id, user_id, score)
+             VALUES ($1, $2, $3) RETURNING *;
+             `;
+            const values = [quizId, result.rows[0].id, score];
+
+            db.query(queryString, values)
+              .then(data => {
+                console.log("Writing to attempt table...  Mhahaha");
+                //redirect to results page results/userid/
+              })
+              .catch(err => {
+                res
+                  .status(500)
+                  .json({ error: err.message });
+              });
+          })
+          .catch((err) => {
+            console.log(err.message);
+          });
+      });
+   });
   return router;
-  };
+  }
