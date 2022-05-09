d55b47883a09bc2e52a67c9992ec557406155ef7
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-09 13:40:09 -0400

    Update to fix bug with auto-increment of primary key in tables

--- db/seeds/01_users_seeds.sql
+++ db/seeds/01_users_seeds.sql
@@ -1,7 +1,7 @@
-INSERT INTO users (id, name, email, password, join_date)
+INSERT INTO users (name, email, password, join_date)
 VALUES
-(1, 'John Doe', 'johndoe@gmail.com', 'password', '2018-02-12'),
-(2, 'Jane Doe', 'janedoe@gmail.com', 'password', '2021-04-06'),
-(3, 'Anne Smith', 'smitty@yahoo.com', 'password', '2021-06-25'),
-(4, 'Patricia Anderson', 'pattya@hotmail.com', 'password', '2021-10-28'),
-(5, 'Wade Johnson', 'wade@icloud.com', 'password', '2022-01-01');
+('John Doe', 'johndoe@gmail.com', 'password', '2018-02-12'),
+('Jane Doe', 'janedoe@gmail.com', 'password', '2021-04-06'),
+('Anne Smith', 'smitty@yahoo.com', 'password', '2021-06-25'),
+('Patricia Anderson', 'pattya@hotmail.com', 'password', '2021-10-28'),
+('Wade Johnson', 'wade@icloud.com', 'password', '2022-01-01');
