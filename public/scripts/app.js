88d7bb990231f5441d8bdb36e43a150910943b6d
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-10 20:39:00 -0400

    Add functionality to copy to clipboard

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,15 @@
 // Client facing scripts here
+
+$(document).ready(function () {
+//when user clicks to copy link to clipboard
+  $("#copy-link").on("click", function () {
+    /* Get the div containing the url */
+    const shareURL = document.getElementById("shareLink");
+
+    //if the browser supports navigator, create a promise to copy the link to the clipboard
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(shareURL.innerHTML)
+        .then(() => alert("Link copied to clipboard"));
+    }
+  });
+});
