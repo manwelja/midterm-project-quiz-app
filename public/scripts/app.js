a5b63d77ca66351ff0e00e0371e0d09891f357db
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-11 10:52:19 -0400

    Fix issue with copy to clipboard link

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,9 @@
-// Client facing scripts here
+const myFunction = function(url) {
+  const shareURL = "http://localhost:8080/takeQuiz/" + url;
+  //if the browser supports navigator, create a promise to copy the link to the clipboard
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(shareURL)
+      .then(() => alert("Link copied to clipboard"));
+  }
+};
 
-$(document).ready(function () {
-//when user clicks to copy link to clipboard
-  $("#copy-link").on("click", function () {
-    /* Get the div containing the url */
-    const shareURL = document.getElementById("shareLink");
-
-    //if the browser supports navigator, create a promise to copy the link to the clipboard
-    if (navigator.clipboard) {
-      navigator.clipboard.writeText(shareURL.innerHTML)
-        .then(() => alert("Link copied to clipboard"));
-    }
-  });
-});
